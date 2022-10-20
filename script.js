a1644cfc8f1374cde3129b1ef19758dc43c31aed
Fortunedan2 <fortunedan2@gmail.com>
2022-10-20 08:08:54 +0100

    Fix play round

--- script.js
+++ script.js
@@ -6,3 +6,24 @@ function getComputerChoice(computerChoice = ['rock', 'paper', 'scissors']) {
 // console.log(getComputerChoice());
 
     //let getPlayerChoice = prompt(("Choose your weapon: "));
+   
+    const playerSelection = "rock";
+    const computerSelection = getComputerChoice();
+    
+function playRound() {
+    
+    if (playerSelection == "rock") {
+        message = "You lose! Paper beats Rock";
+    
+    return message
+ }
+}
+
+game = playRound(playerSelection, computerSelection);
+
+for (let i = 0; i < 5; i++) {
+    message = "Try again!";
+}
+
+
+console.log(game);

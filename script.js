1642d5b636a0b80014fff9daed234c2be2b141e0
Fortunedan2 <fortunedan2@gmail.com>
2022-10-22 23:20:42 +0100

    Rename lengthy variable names

--- script.js
+++ script.js
@@ -1,49 +1,39 @@
 const choices = ['rock', 'paper', 'scissors'];
-const beats = {'rock': 'paper', 'paper': 'scissors', 'scissors': 'rock'};
 
 function computerChoice() {
    return choices[Math.floor(Math.random() * choices.length)];
 }
-
-// console.log(computerChoice());
-
-    
+ 
 function playRound() {
-    const getPlayerChoice = prompt("Choose your weapon: ").toLocaleLowerCase();
+    const human = prompt("Choose your weapon: ").toLocaleLowerCase();
    
-    console.log("You chose " + getPlayerChoice);
+    console.log("You chose " + human);
 
-    const computerSelection = computerChoice();
-    console.log("Computer chose " + computerSelection);
+    const computerPlay = computerChoice();
+    console.log("Computer chose " + computerPlay);
     
-    if (getPlayerChoice == computerSelection) {
+    if (human == computerPlay) {
         console.log("Oh! It's a tie");
  }  
- 
-    else if (getPlayerChoice == "rock" && computerSelection == "paper") {
+    else if (human == "rock" && computerPlay == "paper") {
         console.log('You lose! Paper beats rock');
  }
-
-    else if (getPlayerChoice == "paper" && computerSelection == "rock") {
+    else if (human == "paper" && computerPlay == "rock") {
         console.log('You win! Paper beats rock');
 }
-
-    else if (getPlayerChoice == "paper" && computerSelection == "scissors") {
+    else if (human == "paper" && computerPlay == "scissors") {
         console.log('You lose! Scissors beats Paper');
 }
-
-    else if (getPlayerChoice == "scissors" && computerSelection == "paper") {
+    else if (human == "scissors" && computerPlay == "paper") {
         console.log('You win! Scissors beats Paper');
 }
-
-    else if (getPlayerChoice == "scissors" && computerSelection == "rock") {
+    else if (human == "scissors" && computerPlay == "rock") {
         console.log('You lose! Rock beats scissors');
 }
-    else if (getPlayerChoice == "rock" && computerSelection == "scissors") {
+    else if (human == "rock" && computerPlay == "scissors") {
         console.log('You win! Rock beats scissors');
 }
-
-    else if (getPlayerChoice != "rock", "paper", "scissors") {
+    else if (human != "rock", "paper", "scissors") {
         alert('Seems you made a wrong choice');
     }
 } 
